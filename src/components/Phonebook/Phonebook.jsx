77b23545a6ed4921e37752e556f515b8da4b9e7d
Mary <32392607+MartFrida@users.noreply.github.com>
2023-12-19 18:45:03 +0100

    added filters

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -1,19 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { StyledWrapper } from './Phonebook.styled';
 import { FilterUsers } from 'components/FilterUsers/FilterUsers';
 import { ContactList } from 'components/ContactList/ContactList';
 import { ContactForm } from 'components/ContactForm/ContactForm';
 import { useDispatch, useSelector } from 'react-redux';
-import { addContact, deleteContact } from '../../redux/contacts/contactsSlice';
+import { addContact, deleteContact, setFilter } from '../../redux/contacts/contactsSlice';
 import { nanoid } from '@reduxjs/toolkit';
 
 export const Phonebook = () => {
 
-  const [filter, setFilter] = useState('');
-
+  const filter = useSelector(state => state.contactsData.filter)
   const contacts = useSelector(state => state.contactsData.contacts)
   const dispatch = useDispatch()
-  console.log(contacts)
+  // console.log(contacts)
+
   const handleAddUser = ({ name, number }) => {
     const isExist = contacts.some((item) => item.name === name)
     if (isExist) {
@@ -21,36 +21,20 @@ export const Phonebook = () => {
       return
     }
     const newContact = { id: nanoid(), name, number }
-
     dispatch(addContact(newContact))
-    // setContacts(prevState => [...prevState, { id: nanoid(), name, number }])
   }
 
   const handleSetFilter = (e) => {
-    setFilter(e.target.value)
+    dispatch(setFilter(e.target.value))
   }
 
   const getFilteredContacts = () => {
-    return contacts
-    // return contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()))
+    return contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()))
   }
 
   const handleDeleteUser = (id) => {
-    // setContacts(prevState => prevState.filter(contact => contact.id !== id))
     dispatch(deleteContact(id))
   }
-
-  // useEffect(() => {
-  //   const handleContacts = JSON.parse(window.localStorage.getItem('CONTACTS_DATA'))
-  //   if (handleContacts?.length) {
-  //     setContacts(handleContacts)
-  //   }
-  // }, [])
-
-  // useEffect(() => {
-  //   window.localStorage.setItem('CONTACTS_DATA', JSON.stringify(contacts))
-  // }, [contacts])
-
 
   return (
     <StyledWrapper >
--- src/redux/contacts/contactsSlice.js
+++ src/redux/contacts/contactsSlice.js
@@ -1,10 +1,12 @@
-import { createSlice, nanoid } from "@reduxjs/toolkit"
+import { createSlice } from "@reduxjs/toolkit"
 
 const initialState = {
-  contacts: [{ id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' }],
+  contacts: [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ],
   filter: '',
 }
 
@@ -14,28 +16,18 @@ const contactsSlice = createSlice({
   reducers:
   {
     addContact:
-      // {
-      //   prepare: (name, number) => {
-      //     return {
-      //       payload: {
-      //         id: nanoid(),
-      //         name,
-      //         number,
-      //       }
-      //     }
-      //   },
-      //   reducer: (state, { payload }) => {
-      //     state.contacts.push(payload)
-      //   }
-      // },
       (state, { payload }) => {
         state.contacts.push(payload)
       },
     deleteContact: (state, action) => {
       state.contacts = state.contacts.filter(item => item.id !== action.payload)
-    }
+    },
+    setFilter: (state, { payload }) => {
+      state.filter = payload
+    },
+
   }
 })
 
-export const { addContact, deleteContact } = contactsSlice.actions
+export const { addContact, deleteContact, setFilter } = contactsSlice.actions
 export const contactsReducer = contactsSlice.reducer
